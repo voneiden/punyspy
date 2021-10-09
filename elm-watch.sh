b08e187b990b6f875207c569606384eaf3170d6e
Matti Eiden <matti.eiden@bitfactor.fi>
2021-10-10 00:07:57 +0300

    Add --debug mode

--- elm-watch.sh
+++ elm-watch.sh
@@ -1,4 +1,4 @@
 while true; do
-    find src/ -name "*.elm" | entr -d elm make src/Main.elm --output=app/punyspy.js;
+    find src/ -name "*.elm" | entr -d elm make src/Main.elm --output=app/punyspy.js --debug;
     sleep 0.5;
 done
